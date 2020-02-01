51e61c65c92ba04d2719c091627394164acd0e16
wenksi <silvan.wenk@gmail.com>
2020-02-01 16:54:52 +0100

    Add basic layout of products

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        this.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: Icon(Icons.favorite_border),
+          onPressed: () {},
+        ),
+        title: Text(
+          this.title,
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.black54,
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
