69da112044ac98b51ff968b6e55eb47ceb87d5b6
wenksi <silvan.wenk@gmail.com>
2020-02-11 11:23:21 +0100

    Store user details in Auth Provider

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
 import 'package:flutter_complete_guide/screens/product_details_screen.dart';
+import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
 import 'package:flutter_complete_guide/screens/user_products_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -31,24 +32,26 @@ class MyApp extends StatelessWidget {
           value: Auth(),
         )
       ],
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-          textTheme: TextTheme(
-            title: TextStyle(color: Colors.white),
+      child: Consumer<Auth>( //Whenever Auth changes, this Consumer will reload the MaterialApp.
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+            textTheme: TextTheme(
+              title: TextStyle(color: Colors.white),
+            ),
           ),
+          home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          routes: {
+            ProductDetailsScreen.route: (ctx) => ProductDetailsScreen(),
+            CartScreen.route: (ctx) => CartScreen(),
+            OrdersScreen.route: (ctx) => OrdersScreen(),
+            UserProductsScreen.route: (ctx) => UserProductsScreen(),
+            EditProductScreen.route: (ctx) => EditProductScreen(),
+          },
         ),
-        home: AuthScreen(),
-        routes: {
-          ProductDetailsScreen.route: (ctx) => ProductDetailsScreen(),
-          CartScreen.route: (ctx) => CartScreen(),
-          OrdersScreen.route: (ctx) => OrdersScreen(),
-          UserProductsScreen.route: (ctx) => UserProductsScreen(),
-          EditProductScreen.route: (ctx) => EditProductScreen(),
-        },
       ),
     );
   }
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -11,6 +11,19 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
+  bool get isAuth {
+    return this.token != null;
+  }
+
+  String get token {
+    if (this._expiryDate != null &&
+        this._expiryDate.isAfter(DateTime.now()) &&
+        this._token != null) {
+      return this._token;
+    }
+    return null;
+  }
+
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     try {
@@ -29,7 +42,12 @@ class Auth with ChangeNotifier {
         if (responseData['error'] != null) {
           throw HttpException(responseData['error']['message']);
         }
+        this._token = responseData['idToken'];
+        this._userId = responseData['localId'];
+        this._expiryDate = DateTime.now()
+            .add(Duration(seconds: int.parse(responseData['expiresIn'])));
       });
+      this.notifyListeners();
     } catch (error) {
       throw error;
     }
