86126a76e17964afdd43a24a38a7acb38bf8b1bd
wenksi <silvan.wenk@gmail.com>
2020-02-11 11:05:35 +0100

    Use secret file to hide keys

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,30 +2,34 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_complete_guide/models/http_exception.dart';
+import 'package:flutter_complete_guide/secret-loader.dart';
+import 'package:flutter_complete_guide/secret.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  static const _apiKey = 'AIzaSyAjLL0wzDncPmk7FkpiJiTvroBOqEL4l2s';
   String _token;
   DateTime _expiryDate;
   String _userId;
 
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
-    final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_apiKey';
     try {
-      final response = await http.post(url,
-          body: json.encode({
-            'email': email,
-            'password': password,
-            'returnSecureToken': true,
-          }));
-      final responseData = json.decode(response.body);
-      print(responseData);
-      if (responseData['error'] != null) {
-        throw HttpException(responseData['error']['message']);
-      }
+      Future<Secret> secret = SecretLoader(secretPath: "secrets.json").load();
+      await secret.then((secret) async {
+        final url =
+            'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=${secret.apiKey}';
+        final response = await http.post(url,
+            body: json.encode({
+              'email': email,
+              'password': password,
+              'returnSecureToken': true,
+            }));
+        final responseData = json.decode(response.body);
+        print(responseData);
+        if (responseData['error'] != null) {
+          throw HttpException(responseData['error']['message']);
+        }
+      });
     } catch (error) {
       throw error;
     }
