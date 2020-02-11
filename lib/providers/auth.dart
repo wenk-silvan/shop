4aeba99160cf1d7ff8872abb972a1dc26dfda288
wenksi <silvan.wenk@gmail.com>
2020-02-11 10:20:25 +0100

    Implement log in process

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,17 +4,29 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
+  static const _apiKey = 'AIzaSyAjLL0wzDncPmk7FkpiJiTvroBOqEL4l2s';
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjLL0wzDncPmk7FkpiJiTvroBOqEL4l2s';
-    final response = await http.post(url, body: json.encode({
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_apiKey';
+    final response = await http.post(url,
+        body: json.encode({
           'email': email,
           'password': password,
           'returnSecureToken': true,
-    }));
+        }));
     print(json.decode(response.body));
+    return response.body;
+  }
+
+  Future<void> login(String email, String password) async {
+    return this._authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<void> signup(String email, String password) async {
+    return this._authenticate(email, password, 'signUp');
   }
 }
